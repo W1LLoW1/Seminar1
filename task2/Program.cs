d473fbf425a1e3ee65ea75af9d9cde30bce36345
W1LLoW <w1llowgod.1@gmail.com>
2024-04-19 00:42:56 +0600

    sem1:task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+
+while (count <= num)
+{
+    Console.Write(count + " ");
+    count++;
+}
