62979a71564fa9059b020468aeeb429ed64a4704
W1LLoW <w1llowgod.1@gmail.com>
2024-04-19 00:19:20 +0600

    First task completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string num1str = Console.ReadLine();
+// int num = Convert.ToInt32(num1str);
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число = квадрат второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
+
+
+
