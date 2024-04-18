84a70a78c7a9b2e72090c701252c5e41d1dd4d58
W1LLoW <w1llowgod.1@gmail.com>
2024-04-19 01:18:41 +0600

    sem1:task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10; //Остаток от деления (456 % 10 = 450+6)
+    int result = firstDigit + lastDigit;
+
+    Console.WriteLine($"Сумма первой и последней цифры = {result}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
